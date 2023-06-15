00bfb2024305a239ded8f3f85432c79d67b156b3
youngjuning <1003719811@qq.com>
2023-06-15 14:05:25 +0800

    chore: 紫竹 docs

--- .dumirc.ts
+++ .dumirc.ts
@@ -12,7 +12,7 @@ export default defineConfig({
       twitter: 'https://twitter.com/luozhu2021'
     },
     hd: { rules: [] },
-    footer: '<a href="/sitemap.xml" target="_blank">站点地图</a> | <a href="https://github.com/zizhu-docs" target="_blank">紫竹翻译计划</a> | <a href="https://t.zsxq.com/0fDyODCQs" target="_blank">紫竹赚美刀</a><br/>Made with ❤️ by <a href="https://youngjuning.js.org" target="_blank">洛竹</a>',
+    footer: '<a href="/sitemap.xml" target="_blank">站点地图</a> | <a href="/zizhudocs" target="_blank">紫竹翻译计划</a> | <a href="https://t.zsxq.com/0fDyODCQs" target="_blank">紫竹赚美刀</a><br/>Made with ❤️ by <a href="https://youngjuning.js.org" target="_blank">洛竹</a>',
   },
   theme: {
     '@c-primary': '#e5743f',
