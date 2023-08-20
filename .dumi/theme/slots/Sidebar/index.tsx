80b14e21915fefc8a100a53357f509eeb254de38
紫竹 <1003719811@qq.com>
2023-08-20 12:27:18 +0800

    Update index.tsx

--- .dumi/theme/slots/Sidebar/index.tsx
+++ .dumi/theme/slots/Sidebar/index.tsx
@@ -30,7 +30,11 @@ const Sidebar: FC = () => {
       ))}
       <Adsense
         className="adsbygoogle"
-        style={{display: "block" }} data-ad-client="ca-pub-7029815294762181" data-ad-slot="9692865372" data-ad-format="auto" data-full-width-responsive="true"
+        style={{display: "block" }}
+        data-ad-client="ca-pub-7029815294762181"
+        data-ad-slot="9225510179"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
       />
     </div>
   );
