495e3fffbdc7d9aa0b63fcb12d42dfc91316b216
Aaron Young <luozhu2021@gmail.com>
2025-04-24 08:29:06 +0800

    feat: add funding choices script for Google AdSense in DocLayout

--- .dumi/theme/layouts/DocLayout/index.tsx
+++ .dumi/theme/layouts/DocLayout/index.tsx
@@ -72,6 +72,7 @@ const DocLayout: FC = () => {
           ))}
         {hostname && <link rel="canonical" href={hostname + pathname} />}
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7029815294762181" crossorigin="anonymous"></script>
+        <script async src="https://fundingchoicesmessages.google.com/i/pub-7029815294762181?ers=1"></script><script>(function() {function signalGooglefcPresent() { if (!window.frames['googlefcPresent']) { if (document.body) { const iframe = document.createElement('iframe'); iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;'; iframe.style.display = 'none'; iframe.name = 'googlefcPresent'; document.body.appendChild(iframe); } else { setTimeout(signalGooglefcPresent, 0); } } signalGooglefcPresent();}})();</script>
         <script async src="/busuanzi.pure.min.js" crossorigin="anonymous"></script>
       </Helmet>
       <Header />
